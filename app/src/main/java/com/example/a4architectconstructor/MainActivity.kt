cb02eb072684d6b5c9a9e20327138573b0e0fdfc
Oksana <demskaya01@gmail.com>
2021-06-27 23:52:42 +0300

    change number of floors and roof window

--- app/src/main/java/com/example/a4architectconstructor/MainActivity.kt
+++ app/src/main/java/com/example/a4architectconstructor/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.a4architectconstructor
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.ImageView
+import android.widget.NumberPicker
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbox.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +23,35 @@ class MainActivity : AppCompatActivity() {
             else {
                 toolbox.visibility = View.GONE
             }
+        }
+
+        roof_window_checkbox.setOnCheckedChangeListener{ buttonView, isChecked ->
+            roof_window.visibility = if (isChecked) View.VISIBLE else View.GONE
+        }
+
+        var stage_picker = findViewById<NumberPicker>(R.id.stage_picker)
+        stage_picker.minValue = 1
+        stage_picker.maxValue = 3
+        stage_picker.value = 3
+        stage_picker.setOnValueChangedListener{picker, oldVal, newVal ->
+            setStageCount(newVal)
+        }
+
+        
+    }
+
+    fun setStageCount(newVal: Int) {
+        if (newVal == 1) {
+            center_window3.visibility = View.GONE
+            center_window.visibility = View.GONE
+        }
+        else if (newVal == 2) {
+            center_window3.visibility = View.GONE
+            center_window.visibility = View.VISIBLE
+        }
+        else {
+            center_window3.visibility = View.VISIBLE
+            center_window.visibility = View.VISIBLE
         }
     }
 }
